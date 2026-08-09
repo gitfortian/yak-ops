9c23c92562bb80d99b5f886673c0e87d000d1502
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-09 20:19:35 +0800

    refactor(workflow): remove legacy definition create request

--- yak-ops-business/yak-ops-business-workflow/src/main/java/io/yak/ops/business/workflow/model/WorkflowDefinitionCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package io.yak.ops.business.workflow.model;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-/** 创建工作流基础定义。 */
-public record WorkflowDefinitionCreateRequest(
-    @NotBlank @Size(max = 100) String name,
-    @Size(max = 500) String description) {}
