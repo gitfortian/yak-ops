19716fe493bc628dc01c5450b3019ccc8520ca10
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-09 20:00:21 +0800

    refactor(workflow): add workflow execution dao

---
+++ yak-ops-business/yak-ops-business-workflow/src/main/java/io/yak/ops/business/workflow/dao/WorkflowExecutionDao.java
@@ -0,0 +1,33 @@
+package io.yak.ops.business.workflow.dao;
+
+import io.yak.ops.common.bean.po.workflow.WorkflowExecutionPO;
+import io.yak.ops.common.bean.po.workflow.WorkflowNodeAttemptPO;
+import io.yak.ops.common.bean.po.workflow.WorkflowNodeExecutionPO;
+import java.util.List;
+
+/** 工作流执行聚合与运行索引数据访问接口。 */
+public interface WorkflowExecutionDao {
+  WorkflowExecutionPO selectExecution(String executionId);
+
+  List<WorkflowNodeExecutionPO> selectNodeExecutions(String executionId);
+
+  List<WorkflowNodeAttemptPO> selectNodeAttempts(String executionId);
+
+  int upsertExecution(WorkflowExecutionPO execution);
+
+  int upsertNodeExecution(WorkflowNodeExecutionPO nodeExecution);
+
+  int upsertNodeAttempt(WorkflowNodeAttemptPO attempt);
+
+  int updateExecution(WorkflowExecutionPO execution);
+
+  List<String> selectExecutionIds();
+
+  List<String> selectRecoverableExecutionIds();
+
+  String selectEffectiveRuntimeMetadata(String executionId);
+
+  WorkflowNodeAttemptPO selectAttempt(String attemptId);
+
+  int bindExternalExecution(String attemptId, String externalExecutionId);
+}
