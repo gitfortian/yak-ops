597a2d36f98d7a3a83ef6a5e2d86f54c3f89cfae
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-10 19:05:20 +0800

    refactor(data-development): remove SQL execution mapper

--- yak-ops-business/yak-ops-business-data-development/src/main/java/io/yak/ops/business/development/dao/mapper/SqlTaskExecutionMapper.java
+++
@@ -1,6 +0,0 @@
-package io.yak.ops.business.development.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import io.yak.ops.common.bean.po.development.SqlTaskExecutionPO;
-
-public interface SqlTaskExecutionMapper extends BaseMapper<SqlTaskExecutionPO> {}
