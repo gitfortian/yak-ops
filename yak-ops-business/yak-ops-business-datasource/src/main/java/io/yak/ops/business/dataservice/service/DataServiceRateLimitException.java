26debfc7ad6389d110e93f40d76a381189f041b6
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-15 21:03:42 +0800

    feat: retain rate limited caller identity

--- yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/dataservice/service/DataServiceRateLimitException.java
+++ yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/dataservice/service/DataServiceRateLimitException.java
@@ -6,7 +6,30 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
 public class DataServiceRateLimitException extends RuntimeException {
 
-  public DataServiceRateLimitException(String message) {
+  private final Long apiKeyId;
+  private final String apiKeyName;
+  private final String apiKeyPrefix;
+
+  public DataServiceRateLimitException(
+      String message,
+      Long apiKeyId,
+      String apiKeyName,
+      String apiKeyPrefix) {
     super(message);
+    this.apiKeyId = apiKeyId;
+    this.apiKeyName = apiKeyName;
+    this.apiKeyPrefix = apiKeyPrefix;
+  }
+
+  public Long apiKeyId() {
+    return apiKeyId;
+  }
+
+  public String apiKeyName() {
+    return apiKeyName;
+  }
+
+  public String apiKeyPrefix() {
+    return apiKeyPrefix;
   }
 }
