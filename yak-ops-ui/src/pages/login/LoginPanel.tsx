556c79fad03fcbe930aba74d3ee3b5b3aa932455
weifuwan <295227940@qq.com>
2026-08-25 17:37:54 +0800

    update

--- yak-ops-ui/src/pages/login/LoginPanel.tsx
+++ yak-ops-ui/src/pages/login/LoginPanel.tsx
@@ -85,7 +85,7 @@ function FloatingInput({
 
 function ValidationMessage({ children }: { children: string }) {
   return (
-    <span className="inline-flex h-[18px] items-center gap-1.5 align-middle leading-[18px]">
+    <span className="inline-flex h-[18px] items-center gap-1.5 align-middle leading-[18px]" style={{marginBottom: 8}}>
       <ExclamationCircleOutlined className="flex shrink-0 items-center text-[12px] leading-none [&_svg]:block" />
       <span className="leading-[18px]">{children}</span>
     </span>
