ba4978843891ec81f9f1716df128ff1ac1f6ea58
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-27 16:17:07 +0800

    feat(ui): export filter switch

--- yak-ops-ui/src/components/ui/index.ts
+++ yak-ops-ui/src/components/ui/index.ts
@@ -1,5 +1,10 @@
 export { default as YakButton } from '../YakButton';
 export type { YakButtonProps } from '../YakButton';
 export { default as YakEmpty } from '../YakEmpty';
+export { default as YakFilterSwitch } from '../YakFilterSwitch';
+export type {
+  YakFilterSwitchOption,
+  YakFilterSwitchProps,
+} from '../YakFilterSwitch';
 export { default as YakTab } from '../YakTab';
 export type { YakTabProps } from '../YakTab';
