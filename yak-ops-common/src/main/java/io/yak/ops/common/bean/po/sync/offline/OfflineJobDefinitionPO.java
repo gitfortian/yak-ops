26db220349e82b7990c0393de0e1dc7ecf35d217
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-01 19:25:03 +0800

    feat(sync): map offline editor metadata column

--- yak-ops-common/src/main/java/io/yak/ops/common/bean/po/sync/offline/OfflineJobDefinitionPO.java
+++ yak-ops-common/src/main/java/io/yak/ops/common/bean/po/sync/offline/OfflineJobDefinitionPO.java
@@ -35,6 +35,7 @@ public class OfflineJobDefinitionPO {
   private Integer retryMaxAttempts;
   private Integer retryBackoffSeconds;
   @ToString.Exclude @TableField(updateStrategy = FieldStrategy.ALWAYS) private String notificationConfigJson;
+  @ToString.Exclude @TableField(updateStrategy = FieldStrategy.ALWAYS) private String editorMetaJson;
   @TableField(updateStrategy = FieldStrategy.ALWAYS) private LocalDateTime scheduleLastFireTime;
   @TableField(updateStrategy = FieldStrategy.ALWAYS) private LocalDateTime scheduleNextFireTime;
   private Integer version;
