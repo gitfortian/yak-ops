df7f3b193592b85fbc495de10df005a6f268341d
weifuwan <295227940@qq.com>
2026-09-07 15:17:49 +0800

    update

--- yak-ops-ui/src/components/data-source/icons/DorisIcon.tsx
+++ yak-ops-ui/src/components/data-source/icons/DorisIcon.tsx
@@ -1,9 +1,7 @@
 // SVG 文件内容（你可以直接复制 SVG 代码或者引入 SVG 文件）
-const DorisIcon = ({height = '24', width = '24'}) => {
+const DorisIcon = ({height = 24, width = 24}) => {
   return (
     <svg
-      t="1761630566119"
-      class="icon"
       viewBox="0 0 1024 1024"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
